a9084c6274e3c5cfcc5693089ec865f607d28832
xiaoxustudio <xiaoxustudio@foxmail.com>
2026-06-12 00:16:51 +0800

    refactor(local): split and merge the localisation script files

---
+++ Project/Script/local/export-language-window.js
@@ -0,0 +1,85 @@
+﻿'use strict'
+
+// ******************************** 导出语言包窗口 ********************************
+
+const ExportLanguage = {
+	// methods
+	initialize: null,
+	open: null,
+	exportLanguagePack: null,
+	stringifyLanguagePack: null,
+	// events
+	confirm: null
+}
+
+// 初始化
+ExportLanguage.initialize = function () {
+	// 侦听事件
+	$('#exportLanguage-confirm').on('click', this.confirm)
+}
+
+// 打开窗口
+ExportLanguage.open = function () {
+	Window.open('exportLanguage')
+
+	// 创建语言选项
+	const items = []
+	const none = { name: Local.get('common.none'), value: '' }
+	for (const language of Data.config.localization.languages) {
+		items.push({
+			name: Local.get('languages.' + language.name),
+			value: language.name
+		})
+	}
+	$('#exportLanguage-first').loadItems(items)
+	$('#exportLanguage-first').writeDefault()
+	$('#exportLanguage-second').loadItems([none, ...items])
+	$('#exportLanguage-second').writeDefault()
+}
+
+// 导出语言包
+ExportLanguage.exportLanguagePack = function (first, second) {
+	const pack = {}
+	// 加载文本到映射表
+	const loadText = (items) => {
+		for (const item of items) {
+			if (item.class === 'folder') {
+				loadText(item.children)
+			} else {
+				const { id, contents } = item
+				pack[id] = contents[first] || contents[second] || ''
+			}
+		}
+	}
+	loadText(Data.localization.list)
+	return this.stringifyLanguagePack(pack)
+}
+
+// 字符串化语言包
+ExportLanguage.stringifyLanguagePack = function (map) {
+	const entries = Object.entries(map)
+	const length = entries.length
+	const strings = new Array(length)
+	for (let i = 0; i < length; i++) {
+		const [id, text] = entries[i]
+		strings[i] = '$' + id + '\n' + text
+	}
+	return strings.join('\n\n')
+}
+
+// 确定按钮 - 鼠标点击事件
+ExportLanguage.confirm = function (event) {
+	Window.close('exportLanguage')
+	const dialogs = Editor.config.dialogs
+	const first = $('#exportLanguage-first').read()
+	const second = $('#exportLanguage-second').read()
+	File.showSaveDialog({
+		defaultPath: Path.resolve(dialogs.export, first + '.txt')
+	}).then(({ filePath }) => {
+		if (filePath) {
+			dialogs.export = Path.slash(Path.dirname(filePath))
+			const string = ExportLanguage.exportLanguagePack(first, second)
+			return FSP.writeFile(filePath, string)
+		}
+	})
+}
---
+++ Project/Script/local/local-object.js
@@ -0,0 +1,161 @@
+﻿'use strict'
+
+// ******************************** 游戏本地化对象 ********************************
+
+const GameLocal = {
+	// properties
+	active: '',
+	language: '',
+	refRegexp: /<ref:([0-9a-f]{16})>/g,
+	langRemap: {
+		'zh-HK': 'zh-TW',
+		'zh-SG': 'zh-TW'
+	},
+	// methods
+	initialize: null,
+	setLanguage: null,
+	getLanguage: null,
+	get: null,
+	replace: null,
+	reloadLanguages: null,
+	// events
+	datachange: null
+}
+
+// 初始化
+GameLocal.initialize = function () {
+	// 设置默认语言
+	this.setLanguage(Data.config.localization.default)
+
+	// 侦听事件
+	window.on('datachange', this.datachange)
+}
+
+// 设置语言
+GameLocal.setLanguage = async function (language) {
+	if (this.language !== language || language === 'auto') {
+		const languages = Data.config.localization.languages
+		let active = language
+		if (active === 'auto') {
+			active = this.getLanguage()
+		}
+		let settings = languages.find((lang) => lang.name === active)
+		if (!settings)
+			settings = languages[0] ?? { name: active, font: '', scale: 1 }
+		try {
+			this.active = settings.name
+			this.language = language
+			window.dispatchEvent(new Event('localizationchange'))
+			Printer.setLanguageFont(settings.font)
+			Printer.setSizeScale(settings.scale)
+			Printer.setWordWrap(
+				['zh-CN', 'zh-TW', 'ja', 'ko'].includes(active)
+					? 'break'
+					: 'keep'
+			)
+		} catch (error) {
+			Log.throw(error)
+		}
+	}
+}
+
+// 获取语言
+GameLocal.getLanguage = function () {
+	const languages = Data.config.localization.languages.map(
+		(lang) => lang.name
+	)
+	let nLanguage = navigator.language
+	// 重映射本地语言
+	if (this.langRemap[nLanguage]) {
+		nLanguage = this.langRemap[nLanguage]
+	}
+	let language = languages[0] ?? nLanguage
+	let matchedWeight = 0
+	const sKeys = nLanguage.split('-')
+	for (const key of languages) {
+		const dKeys = key.split('-')
+		if (sKeys[0] === dKeys[0]) {
+			let weight = 0
+			for (let sKey of sKeys) {
+				if (dKeys.includes(sKey)) {
+					weight++
+				}
+			}
+			if (matchedWeight < weight) {
+				matchedWeight = weight
+				language = key
+			}
+		}
+	}
+	return language
+}
+
+// 获取本地化文本
+GameLocal.get = function (id) {
+	const map = Data.localization.map
+	return map[id]?.contents[this.active]
+}
+
+// 替换文本内容
+GameLocal.replace = function (text) {
+	return text.replace(this.refRegexp, (match, refId) => {
+		const ref = this.get(refId)
+		return ref !== undefined ? ref : match
+	})
+}
+
+// 重新加载语言
+GameLocal.reloadLanguages = function () {
+	const languages = Data.config.localization.languages.map(
+		(lang) => lang.name
+	)
+	const reload = (items) => {
+		for (const item of items) {
+			if (item.class === 'folder') {
+				reload(item.children)
+			} else {
+				const contents = {}
+				for (const language of languages) {
+					contents[language] = item.contents[language] ?? ''
+				}
+				item.contents = contents
+			}
+		}
+	}
+	reload(Data.localization.list)
+	File.planToSave(Data.manifest.project.localization)
+}
+
+// 数据改变事件
+GameLocal.datachange = function (event) {
+	if (event.key === 'config') {
+		const last = event.last.localization
+		const now = Data.config.localization
+		const lastLanguages = last.languages.map((lang) => lang.name)
+		const nowLanguages = now.languages.map((lang) => lang.name)
+		if (JSON.stringify(lastLanguages) !== JSON.stringify(nowLanguages)) {
+			GameLocal.reloadLanguages()
+			// 如果是自动语言，随着语言列表的变化重新加载
+			if (now.default === 'auto') {
+				GameLocal.setLanguage(now.default)
+				return
+			}
+		}
+		// 默认语言改变时重新设置语言
+		if (last.default !== now.default) {
+			GameLocal.setLanguage(now.default)
+			return
+		}
+		// 当前语言参数改变时更新所有文本打印机
+		const selector = (lang) => lang.name === GameLocal.active
+		const lastSettings = last.languages.find(selector)
+		const nowSettings = now.languages.find(selector)
+		if (
+			JSON.stringify(lastSettings ?? '') !==
+			JSON.stringify(nowSettings ?? '')
+		) {
+			Printer.setLanguageFont(nowSettings?.font ?? Printer.languageFont)
+			Printer.setSizeScale(nowSettings?.scale ?? Printer.sizeScale)
+		}
+	}
+}
