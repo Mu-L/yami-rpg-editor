109552cfc49724ec60415538e7b71a89ec4516e0
xiaoxustudio <xiaoxustudio@foxmail.com>
2026-06-12 00:18:35 +0800

    refactor(log): split and merge the log module and restructure it into separate files

---
+++ Project/Script/log/update-log-window.js
@@ -0,0 +1,59 @@
+﻿'use strict'
+
+// ******************************** 更新日志窗口 ********************************
+
+const UpdateLog = {
+	// properties
+	content: $('#update-log-content'),
+	// methods
+	open: null,
+	update: null,
+	// events
+	windowClosed: null
+}
+
+// 初始化
+UpdateLog.initialize = function () {
+	// 侦听事件
+	$('#update-log').on('closed', this.windowClosed)
+}
+
+// 打开窗口
+UpdateLog.open = function (items = null) {
+	if (items instanceof Array) {
+		Window.open('update-log')
+		this.update(items)
+	} else {
+		Updater.updateIncrementalChanges(Updater.latestProjectVersion)
+	}
+}
+
+// 更新内容
+UpdateLog.update = function (items) {
+	this.content.clear()
+	for (const item of items) {
+		if (item.title) {
+			const title = document.createElement('text')
+			title.textContent = item.title
+			title.addClass('update-log-title')
+			this.content.appendChild(title)
+		}
+		if (item.major) {
+			const major = document.createElement('text')
+			major.textContent = item.major
+			major.addClass('update-log-major')
+			this.content.appendChild(major)
+		}
+		if (item.minor) {
+			const minor = document.createElement('text')
+			minor.textContent = item.minor
+			minor.addClass('update-log-minor')
+			this.content.appendChild(minor)
+		}
+	}
+}
+
+// 窗口 - 已关闭事件
+UpdateLog.windowClosed = function () {
+	UpdateLog.content.clear()
+}
