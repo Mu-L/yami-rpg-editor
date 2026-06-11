aea95d6a13dd7dfc6956a55582cf30453a96ca89
xiaoxustudio <xiaoxustudio@foxmail.com>
2026-06-12 00:07:59 +0800

    refactor(file): split the original file.js into independent file modules

---
+++ Project/Script/file/file-system.js
@@ -0,0 +1,12 @@
+﻿'use strict'
+
+// ******************************** 文件系统 ********************************
+
+const FS = require('fs')
+const FSP = FS.promises
+
+// 重写写入文件方法
+FSP.writeFile = function (path, text, check = false) {
+	const { invoke } = require('electron').ipcRenderer
+	return invoke('write-file', path, text, check)
+}
---
+++ Project/Script/file/path-utils.js
@@ -0,0 +1,19 @@
+﻿'use strict'
+
+// ******************************** 路径工具 ********************************
+
+// 转换至斜杠分隔符
+Path.slash = (function IIFE() {
+	const regexp = /\\/g
+	return function (path) {
+		if (path.indexOf('\\') !== -1) {
+			path = path.replace(regexp, '/')
+		}
+		return path
+	}
+})()
+
+// 获取文件扩展名
+// Path.ext = function (path) {
+//   return path.slice(path.lastIndexOf('.') + 1)
+// }
