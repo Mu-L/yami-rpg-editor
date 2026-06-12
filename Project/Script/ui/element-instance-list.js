7bacb1101657554313c43ca357eac21e263b763e
xiaoxustudio <xiaoxustudio@foxmail.com>
2026-06-12 11:11:53 +0800

    feat(ui): reorganize the module structure of the tool

---
+++ Project/Script/ui/element-instance-list.js
@@ -0,0 +1,26 @@
+﻿'use strict'
+
+// ******************************** 元素实例列表 ********************************
+
+class UIInstanceList extends Array {
+	// 设置属性
+	set(key, value) {
+		const keys = key.split('-')
+		const last = keys.length - 1
+		const property = keys[last]
+		for (const element of this) {
+			let node = element
+			for (let i = 0; i < last; i++) {
+				node = node[keys[i]]
+			}
+			node[property] = value
+		}
+	}
+
+	// 调整大小
+	resize() {
+		for (const element of this) {
+			element.resize()
+		}
+	}
+}
